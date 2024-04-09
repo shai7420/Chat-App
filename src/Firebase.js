1d3fac24014c9dc4040836147a32610182d1ae65
Shaiharan <52232010+shai7420@users.noreply.github.com>
2024-04-09 11:45:21 +0530

    Firebase.js

--- src/Firebase.js
+++ src/Firebase.js
@@ -4,13 +4,13 @@ import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyA-sGvOfQtVt631wYinWAUT3C9e9ZliwnQ",
-    authDomain: "silver-chatz.firebaseapp.com",
-    projectId: "silver-chatz",
-    storageBucket: "silver-chatz.appspot.com",
-    messagingSenderId: "172501285538",
-    appId: "1:172501285538:web:77142605d183d6ccceb5fb",
-    measurementId: "G-6ZT88F2EEV"
+    apiKey: "Your apiKey",
+    authDomain: "Your authDomain",
+    projectId: "Your projectId",
+    storageBucket: "Your storageBucket",
+    messagingSenderId: "Your messagingSenderId",
+    appId: "Your appId",
+    measurementId: "Your measurementId"
 };
 
 export const app = initializeApp(firebaseConfig);
